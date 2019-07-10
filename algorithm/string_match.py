4807a2c6bc55ef246b1b98ac658743f3786a767d
HaiYang Dang <1542938982@qq.com>
2019-07-11 01:06:31 +0800

    Add: string's KMP matching

--- algorithm/string_match.py
+++ algorithm/string_match.py
@@ -15,3 +15,34 @@ def naive_matching(s, sub):
     if i == m:
         return j - 1
     return -1
+
+
+"""KMP算法"""
+
+
+def gen_pnext(p):
+    """生成针对p中各位置i的下一检查位置表，用于KMP算法"""
+    i, k, m = 0, -1, len(p)
+    pnext = [-1] * m
+    while i < m-1:
+        if k == -1 or p[i] == p[k]:
+            i, k = i+1, k+1
+            pnext[i] = k
+        else:
+            k = pnext[i]
+    return pnext
+
+
+def matching_kmp(t, p, pnext):
+    """KMP串匹配"""
+    j, i = 0, 0
+    n, m = len(t), len(m)
+    while j < n and i < m:
+        if i == -1 or t[j] == p[i]:
+            j, i = j+1, i+1
+        else:
+            i = pnext[i]
+    if i == m:
+        return j-i
+    return -1
+
