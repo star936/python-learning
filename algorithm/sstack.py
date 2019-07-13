56e0496131af85344c9415fb8cfe09e22d19a0e3
HaiYang Dang <1542938982@qq.com>
2019-07-13 22:58:13 +0800

    Add class SStack

---
+++ algorithm/sstack.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+
+class StackUnderflow(ValueError):
+    pass
+
+
+class SStack(object):
+    def __init__(self):
+        self._elems = []
+
+    def is_empty(self):
+        return len(self._elems) == 0
+
+    def top(self):
+        if self.is_empty():
+            raise StackUnderflow("in SStack.top")
+        return self._elems[-1]
+
+    def push(self, elem):
+        self._elems.append(elem)
+
+    def pop(self):
+        if self.is_empty():
+            raise StackUnderflow("in SStack.pop")
+        return self._elems.pop()
+
