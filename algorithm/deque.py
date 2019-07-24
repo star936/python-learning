c7a1638a61d04755ac429cd2861d1d9396519021
HaiYang Dang <1542938982@qq.com>
2019-07-24 22:48:39 +0800

    Add deque class

---
+++ algorithm/deque.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+
+class Deque(object):
+    """双端队列"""
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        self.items == []
+
+    def add_front(self, item):
+        self.items.append(item)
+
+    def add_rear(self, item):
+        self.items.insert(0, item)
+
+    def remove_front(self):
+        self.items.pop()
+
+    def remove_rear(self):
+        self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
