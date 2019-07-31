943725572892e528e53c1c4ae4bc9d0240f74389
HaiYang Dang <1542938982@qq.com>
2019-07-31 23:59:10 +0800

    add binary heap

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -36,3 +36,47 @@ class BinaryTree(object):
 
     def get_rootvalue(self):
         return self.key
+
+
+class BinHeap(object):
+    """二叉堆"""
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self, i):
+        """最小堆: 根节点存储最小值"""
+        while i//2 > 0:
+            if self.heapList[i] < self.heapList[i//2]:
+                self.heapList[i//2], self.heapList[i] = self.heapList[i], self.heapList[i//2]
+            i = i//2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
+
+    def percDown(self, i):
+        while (i*2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                self.heapList[i], self.heapList[mc] = self.heapList[mc], self.heapList[i]
+            i = mc
+
+    def minChild(self, i):
+        if i*2 + 1 > self.currentSize:
+            return i*2
+        else:
+            if self.heapList[i*2] < self.heapList[i*2+1]:
+                return i*2
+            else:
+                i*2 + 1
+
+    def delMin(self):
+        value = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return value
+
