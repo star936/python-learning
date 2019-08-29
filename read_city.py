9a557a72d37c5d3c22cc9e4c28dca0b2afb2cb98
Haiyang Dang <1542938982@qq.com>
2019-08-29 19:57:16 +0800

    Added read sqlite data

---
+++ read_city.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+import sqlite3
+import psycopg2
+
+HOST = 'localhost'
+PORT = 5432
+DATABASE = 'test'
+USER = 'root'
+PASSWORD = 'root'
+
+pg_conn = psycopg2.connect(host=HOST,
+                           port=PORT,
+                           user=USER,
+                           password=PASSWORD,
+                           dbname=DATABASE)
+pg_cur = pg_conn.cursor()
+
+pg_sql = "INSERT INTO city(id, country, state, city, zip, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
+conn = sqlite3.connect('City.sqlite')
+
+cursor = conn.cursor()
+
+if __name__ == '__main__':
+    cursor.execute("SELECT * FROM city")
+    values = cursor.fetchall()
+    data = []
+    num = 0
+    for v in values:
+        data.append([v[0], v[1], v[2], v[3], v[4], v[5], v[6]])
+        if len(data) == 1000:
+            num += 1
+            print(num)
+            print(data)
+            pg_cur.executemany(pg_sql, data)
+            pg_conn.commit()
+            data = []
+
+    conn.close()
+    pg_conn.close()
